3dbe53ba20b541dbf14c2a8c3493bb679b92e8a3
Ian Nguyen <iannknguyen@gmail.com>
2016-01-19 01:09:37 -0800

    add remaining count and inflection

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  remaining: Ember.computed('todos.@each.complete', function() {
+    let todos = this.get('todos');
+    return todos.filterBy('complete', false).get('length');
+  }),
+  inflection: Ember.computed('todos.@each.complete', function() {
+    var remaining = this.get('remaining');
+    return (remaining === 1) ? 'item' : 'items';
+  })
 });
