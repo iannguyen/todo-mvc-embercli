ebeefeb33d977db9b11d1144d7534b54dc5a6877
Ian Nguyen <iannknguyen@gmail.com>
2016-01-18 21:25:03 -0800

    define 'editing' property on todo-item, className binding, and toggle

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'li'
+  tagName: 'li',
+  classNameBindings: ['editing'],
+  editing: false,
+  actions: {
+    editTodo() {
+      this.toggleProperty('editing');
+    }
+  }
 });
